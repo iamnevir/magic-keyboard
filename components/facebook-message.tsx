daefc8689c75fb2538d9a2c8a897cfaa92abc5c9
iamnevir <hiromansofaoi@gmail.com>
2023-12-02 00:03:46 +0700

    fix chat

--- components/facebook-message.tsx
+++ components/facebook-message.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { useTheme } from "next-themes";
 import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessage = () => {
-  const { theme } = useTheme();
   return (
     <FacebookProvider appId="1432258124036626" chatSupport>
-      <CustomChat
-        themeColor={theme === "dark" ? "dark" : "white"}
-        pageId="187437071115370"
-        minimized={false}
-      />
+      <CustomChat pageId="187437071115370" minimized={false} />
     </FacebookProvider>
   );
 };
